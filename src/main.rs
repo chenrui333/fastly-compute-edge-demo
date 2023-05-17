ca1be9e40957d688420dda92ec150756df2e6d27
Rui Chen <rui@chenrui.dev>
2023-05-17 17:50:18 -0400

    feat: add allowlist logic
    ```
    $ curl https://fastly-compute-edge-demo.edgecompute.app
    Forbidden, your ip is 2a09:bac5:a6f1:1973::289:47
    ```
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
+use std::net::IpAddr;
 use fastly::http::{header, Method, StatusCode};
 use fastly::{mime, Error, Request, Response};
+use ipnetwork::IpNetwork;
 
 const HTTPBIN: &str = "httpbin";
 
+fn is_ip_in_cidr(ip: IpAddr, cidr: IpNetwork) -> bool {
+  cidr.contains(ip)
+}
+
+fn is_allowed(ip: IpAddr) -> bool {
+  let cidrs: Vec<&str> = vec![
+    "52.119.64.2/32" // hq 4.0 ip
+  ]; // Allowlist of CIDR blocks
+  let allowlist: Vec<IpNetwork> = cidrs.iter().map(|&cidr| cidr.parse().unwrap()).collect();
+  allowlist.iter().any(|&cidr| is_ip_in_cidr(ip, cidr))
+}
+
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
+    match req.get_client_ip_addr() {
+        Some(ip) => {
+            if is_allowed(ip) {
+                println!("client ip: {} is allowed", ip);
+            } else {
+                println!("client ip: {} is not allowed", ip);
+                return Ok(Response::from_status(StatusCode::FORBIDDEN)
+                    .with_content_type(mime::TEXT_HTML_UTF_8)
+                    .with_body(format!("Forbidden, your ip is {}", ip)));
+            }
+        }
+        None => {
+            println!("no client ip");
+        }
+    }
+
     // Filter request methods...
     match req.get_method() {
         // Allow GET and HEAD requests.
